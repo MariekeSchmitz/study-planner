8d21f2bc50b6e19f576dd55485ebce62763d5bfb
marieke schmitz <marieke.schmitz@student.hs-rm.de>
2022-06-19 10:36:33 +0200

    Setup project

---
+++ src/main/java/de/hsrm/mi/swt/SPAsS/App.java
@@ -0,0 +1,26 @@
+package de.hsrm.mi.swt.SPAsS;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ src/main/java/de/hsrm/mi/swt/SPAsS/Main.java
@@ -0,0 +1,11 @@
+package de.hsrm.mi.swt.SPAsS;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(App.class);
+	}
+	
+}
