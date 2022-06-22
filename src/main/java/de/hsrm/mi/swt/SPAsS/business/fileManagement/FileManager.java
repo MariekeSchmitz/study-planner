d9c2fbd325e8439a4cb935e2b673b6eaf90c0059
Fabio Bertels <Fabio.Bertels@german-edge-cloud.io>
2022-06-22 20:25:09 +0200

    reading from and saving to file

--- src/main/java/de/hsrm/mi/swt/SPAsS/business/fileManagement/FileManager.java
+++ src/main/java/de/hsrm/mi/swt/SPAsS/business/fileManagement/FileManager.java
@@ -1,7 +1,14 @@
 package de.hsrm.mi.swt.SPAsS.business.fileManagement;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 
 import de.hsrm.mi.swt.SPAsS.business.planManagement.Plan;
 import de.hsrm.mi.swt.SPAsS.business.restrictionManagement.Validator;
@@ -9,9 +16,11 @@ import de.hsrm.mi.swt.SPAsS.business.restrictionManagement.Validator;
 public class FileManager {
     private String path;
     public String jsonTestString;
+    Plan testPlan;
 
     public FileManager(String path){
         this.path = path;   
+        testPlan = new TestClassGenerator().plan;
     }
 
     public void curriculumScan(){
@@ -20,21 +29,36 @@ public class FileManager {
     public void planScan(){
 
     }
-    public Plan fileRead(){
+    public Plan fileRead(String name){
         Gson gson = new GsonBuilder()
             .registerTypeAdapter(Validator.class, new InterfaceDeserializer<>())
             .create();
-        return gson.fromJson(jsonTestString, Plan.class);
+        try {
+            return gson.fromJson(new FileReader(name+".json"), Plan.class);
+        } catch (JsonSyntaxException | JsonIOException | FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
     }
     public void fileSave(Plan p){
         this.jsonTestString = new Gson().toJson(p);
+        try {
+            FileWriter out = new FileWriter(p.getName()+".json");
+            out.write(jsonTestString);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public void test(){
-		this.fileSave(new TestClassGenerator().plan);
+		this.fileSave(testPlan);
 		System.out.println(this.jsonTestString);
         String s = jsonTestString;
-		Plan p = this.fileRead();
+		Plan p = this.fileRead(testPlan.getName());
         fileSave(p);
         if(s.equals(jsonTestString)) System.out.println("Parser still workinng");
         else System.out.println("you killed the parser");
